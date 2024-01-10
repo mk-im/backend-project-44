477f0e88898ab56f9239f80735cb8ba670183d3e
root <marinkim1999@gmail.com>
2024-01-10 23:52:21 +0300

    fix

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -1,4 +1,4 @@
-import getNumberArray from '../utils.js';
+import { getNumberArray } from '../utils.js';
 import gameLogic from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
